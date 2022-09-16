d887fbb322774d813acb1e55b0f83a2d10aad285
Robin Tapiru <robintapiru0894@gmail.com>
2022-09-16 17:00:05 +0400

    asd

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,4 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
@@ -5,12 +6,12 @@ function Expenses(props) {
 
     return(
         
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem key={props.expense[0].id} title={props.expense[0].title} amount={props.expense[0].amount} date={props.expense[0].date} />
             <ExpenseItem key={props.expense[1].id} title={props.expense[1].title} amount={props.expense[1].amount} date={props.expense[1].date} />
             <ExpenseItem key={props.expense[2].id} title={props.expense[2].title} amount={props.expense[2].amount} date={props.expense[2].date} />
             <ExpenseItem key={props.expense[3].id} title={props.expense[3].title} amount={props.expense[3].amount} date={props.expense[3].date} />
-        </div>
+        </Card>
         
     );
 
